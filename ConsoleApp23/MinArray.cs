b974c9be3eda45c2e3f9b82695a509fff03ed4ff
Tural Taghiyev <turalrt@code.edu.az>
2021-12-11 22:57:50 +0400

    first commit

---
+++ ConsoleApp23/MinArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class MinArray
+    {
+        int[] arr = { 10, 18, 27, 2, 100, 0, -5, 77, -24, 99 };
+        public void checkarray()
+        {
+            int min = arr.Min();
+            int index = Array.IndexOf(arr, min);
+            Console.WriteLine($"min={min}");
+            Console.WriteLine($"index={index}");
+        }
+    }
+}
